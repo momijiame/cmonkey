dc01b012d8d5ef0652b3762a8462ca0c9484e568
momijiame <amedama.ginmokusei@gmail.com>
2013-12-12 23:39:59 +0900

    コマンドラインパーサのテストを追加する

--- cmonkey/tests/__init__.py
+++ cmonkey/tests/__init__.py
@@ -16,17 +16,34 @@ except ImportError:
 class Test_Main(object):
 
     def test_parse_default(self):
-        sys.argv = ['cmonkey', 'command=listUsers']
+        sys.argv = ['cmonkey', '-a', 'foo', '-s', 'bar', 'command=listUsers']
         args = _parse_args()
         eq_(args.entry_point, 'http://localhost:8080/client/api')
-        eq_(args.api_key, None)  # XXX
-        eq_(args.secret_key, None)  # XXX
+        eq_(args.api_key, 'foo')
+        eq_(args.secret_key, 'bar')
         eq_(args.xml_format, False)
-        eq_(args.show_status_code, False)
-        eq_(args.show_headers, False)
-        eq_(args.show_response_body, False)
+        eq_(args.hide_status_code, False)
+        eq_(args.hide_headers, False)
+        eq_(args.hide_response_body, False)
         eq_(args.parameters, ['command=listUsers'])
 
+    def test_parse_error_required_a(self):
+        sys.argv = ['cmonkey', '-s', 'bar', 'command=listUsers']
+        sys.exit = mock.Mock()
+        _parse_args()
+        calls = [
+            mock.call(2)
+        ]
+        sys.exit.assert_has_calls(calls)
+
+    def test_parse_error_required_s(self):
+        sys.argv = ['cmonkey', '-a', 'foo', 'command=listUsers']
+        sys.exit = mock.Mock()
+        _parse_args()
+        calls = [
+            mock.call(2)
+        ]
+        sys.exit.assert_has_calls(calls)
 
 if __name__ == "__main__":
     nose.main(argv=['nosetests', '-s', '-v'], defaultTest=__file__)
