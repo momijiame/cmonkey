0fa5005752ba37871003ed63c26668ce034899b7
momijiame <amedama.ginmokusei@gmail.com>
2013-12-13 00:05:01 +0900

    認証に関わる部分は auth.py に移す

--- cmonkey/tests/__init__.py
+++ cmonkey/tests/__init__.py
@@ -5,27 +5,12 @@ import sys
 import nose
 from nose.tools.trivial import eq_
 
-from cmonkey import _parse_args, SignatureBuilder
+from cmonkey import _parse_args
 
 try:
     import mock
 except ImportError:
     from unittest import mock
-
-
-class Test_SignatureBuilder(object):
-
-    APIKEY = 'B1glHBDDvXwKz4XkLXhd_Hk5-Fp8RZfukbE4shWk2p9nRjPvtMLTtNtawtD1H-a4kh06P0U5eRBELVOl6OAThg'
-    SECRETKEY = 'VpznCS2q7t9-Sd8QJJwW_VLm_IX1g3ua9fMasSyD8jD5XBXso3heVG6_3PUcQi5lVWZXXYKoJwcWukv0V7DvCQ'
-
-    def test_build(self):
-        params = {
-            'command': 'listUsers',
-            'keyword': 'ad',
-        }
-        builder = SignatureBuilder(self.APIKEY, self.SECRETKEY)
-        signature = builder.build(params)
-        eq_(signature, 'QEq3xbEHhBmSfFw4RwVzkWyQYWc%3D')
 
 
 class Test_Main(object):
