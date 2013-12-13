04b0902c7e1b87c4fe329c2596723216752490e7
momijiame <amedama.ginmokusei@gmail.com>
2013-12-14 01:55:31 +0900

    auth.py の内容を client.py に移す

--- cmonkey/client.py
+++ cmonkey/client.py
@@ -1,16 +1,132 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from abc import ABCMeta, abstractmethod
 
-class CloudStackApiClient(object):
+import hmac
+import hashlib
+import base64
 
-    def __init__(self, authenticator):
-        self.authenticator = authenticator
+import six
+from six.moves.urllib import parse
+import requests
+
+
+class AttributeInvokeMixin(object):
 
     def __getattr__(self, name):
         def handle(*args, **kwargs):
-            return self._request(name, kwargs)
+            return self.invoke(name, kwargs)
         return handle
 
-    def _request(self, command, params):
+
+@six.add_metaclass(ABCMeta)
+class ClientBase(AttributeInvokeMixin):
+
+    def __init__(self, endpoint):
+        self.endpoint = endpoint
+        self.session = requests.Session()
+
+    @abstractmethod
+    def invoke(self, command, params):
         pass
+
+    def request(self, method=None, params=None, headers=None, data=None):
+        params = params or {}
+        headers = headers or {}
+        data = data or {}
+        return self.session.request(
+            method or 'GET',
+            self.endpoint,
+            params=params,
+            headers=headers,
+            data=data,
+        )
+
+
+class CookieClient(ClientBase):
+
+    def __init__(self, endpoint, username, password):
+        super(CookieClient, self).__init__(endpoint)
+        self.username = username
+        self.password = password
+
+    def invoke(self, command, params):
+        params['command'] = command
+        session_key = self.login()
+        params['sessionkey'] = session_key
+        return self.request(params=params)
+
+    def login(self):
+        params = {
+            'response': 'json',
+        }
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        data = {
+            'command': 'login',
+            'username': self.username,
+            'password': self._hexdigest(self.password),
+            'domain': '/',
+        }
+        r = self.request('POST', params, headers, data)
+        r_json = r.json()
+        data = r_json['loginresponse']
+        return data['sessionkey']
+
+    def _hexdigest(self, s):
+        m = hashlib.md5()
+        m.update(s.encode())
+        return m.hexdigest()
+
+
+class SignatureBuilder(object):
+
+    def __init__(self, apikey, secretkey):
+        self.apikey = apikey
+        self.secretkey = secretkey
+
+    def build(self, params):
+        if not 'apikey' in [k.lower() for k in params.keys()]:
+            params['apikey'] = self.apikey
+        # URL エンコードする
+        quoted_params = dict([
+            (k, parse.quote(v))
+            for k, v in params.items()
+        ])
+        # lower case にする
+        lower_pairs = [
+            ('%s=%s' % (k, v)).lower()
+            for k, v in quoted_params.items()
+        ]
+        # アルファベット順でソートする
+        sorted_pairs = sorted(lower_pairs)
+        query_string = '&'.join(sorted_pairs)
+        # SHA1 のハッシュを計算する
+        sha1hash = hmac.new(
+            self.secretkey.encode(),
+            query_string.encode(),
+            hashlib.sha1,
+        )
+        digest = sha1hash.digest()
+        # Base64 エンコードする
+        signature = base64.b64encode(digest)
+
+        return signature
+
+
+class SignatureClient(ClientBase):
+
+    def __init__(self, endpoint, apikey, secretkey):
+        super(SignatureClient, self).__init__(endpoint)
+        self.apikey = apikey
+        self.secretkey = secretkey
+        self.signature_builder = SignatureBuilder(apikey, secretkey)
+
+    def invoke(self, command, params):
+        params['command'] = command
+        signature = self.signature_builder.build(params)
+        params['apikey'] = self.apikey
+        params['signature'] = signature
+        return self.request(params=params)
--- cmonkey/tests/test_client.py
+++ cmonkey/tests/test_client.py
@@ -1,16 +1,125 @@
 # -*- coding: utf-8 -*-
 
 import nose
+from nose.tools.trivial import eq_
 
-from cmonkey.auth import DummyAuthenticator
-from cmonkey.client import CloudStackApiClient
+from cmonkey.client import CookieClient, SignatureClient, SignatureBuilder
+import hashlib
+
+try:
+    import mock
+except ImportError:
+    from unittest import mock
 
 
-class Test_CloudStackApiClient(object):
+class Test_SignatureBuilder(object):
 
-    def test_getattr(self):
-        authenticator = DummyAuthenticator()
-        CloudStackApiClient(authenticator)
+    APIKEY = 'B1glHBDDvXwKz4XkLXhd_Hk5-Fp8RZfukbE4shWk2p9nRjPvtMLTtNtawtD1H-a4kh06P0U5eRBELVOl6OAThg'
+    SECRETKEY = 'VpznCS2q7t9-Sd8QJJwW_VLm_IX1g3ua9fMasSyD8jD5XBXso3heVG6_3PUcQi5lVWZXXYKoJwcWukv0V7DvCQ'
+
+    def test_build(self):
+        params = {
+            'command': 'listUsers',
+            'keyword': 'ad',
+        }
+        builder = SignatureBuilder(self.APIKEY, self.SECRETKEY)
+        signature = builder.build(params)
+        eq_(signature, b'QEq3xbEHhBmSfFw4RwVzkWyQYWc=')
+
+
+class Test_CookieClient(object):
+
+    def test_request(self):
+        endpoint = 'http://localhost:8080/client/api'
+        username = 'admin'
+        password = 'password'
+        client = CookieClient(endpoint, username, password)
+        # モックアウト
+        sessionkey = 'hogehoge'
+        client.login = mock.MagicMock(return_value=sessionkey)
+        client.request = mock.Mock()
+        # 実行
+        params = {
+            'account': 'admin',
+            'response': 'json',
+        }
+        client.listUsers(**params)
+        # 検証
+        calls = [
+            mock.call()
+        ]
+        client.login.assert_has_calls(calls)
+        params['command'] = 'listUsers'
+        params['sessionkey'] = sessionkey
+        calls = [
+            mock.call(params=params)
+        ]
+        client.request.assert_has_calls(calls)
+
+    def test_login(self):
+        endpoint = 'http://localhost:8080/client/api'
+        username = 'admin'
+        password = 'password'
+        client = CookieClient(endpoint, username, password)
+        response_mock = mock.Mock()
+        response_mock.json = lambda: {
+            'loginresponse': {
+                'sessionkey': 'hoge',
+            },
+        }
+        client.request = mock.MagicMock(return_value=response_mock)
+        client.login()
+        m = hashlib.md5()
+        m.update(password.encode())
+        return m.hexdigest()
+        params = {
+            'response': 'json',
+        }
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        data = {
+            'command': 'login',
+            'username': username,
+            'password': self._hexdigest(password),
+            'domain': '/',
+        }
+        calls = [
+            mock.call('POST', params, headers, data)
+        ]
+        client.request.assert_has_calls(calls)
+
+    def _hexdigest(self, s):
+        m = hashlib.md5()
+        m.update(s.encode())
+        return m.hexdigest()
+
+
+class Test_SignatureClient(object):
+
+    APIKEY = 'p-dZvP8oknG8RwRuHM_k-pTCqni9wY-_n3mdroNn4Bo9u_hG9FXO39gcjmWWCwClSJgP47-fU3JBh8yCs_Do1A'
+    SECRETKEY = 'HzlToLKEgOenK11URMpy-3arN5ZJ0JqRC-We9FDhcEbKtMIBE71kebVeYCWeLNA7SIZVDpM_yUSM6vtjU7K3MQ'
+    SIGNATURE = b'jhoz5uOKJp8707r9MeMJjCOVLqM='
+
+    def test_request(self):
+        endpoint = 'http://localhost:8080/client/api'
+        client = SignatureClient(endpoint, self.APIKEY, self.SECRETKEY)
+        # モックアウト
+        client.request = mock.Mock()
+        # 実行
+        params = {
+            'account': 'admin',
+            'response': 'json',
+        }
+        client.listUsers(**params)
+        # 検証
+        params['command'] = 'listUsers'
+        params['apikey'] = self.APIKEY
+        params['signature'] = self.SIGNATURE
+        calls = [
+            mock.call(params=params)
+        ]
+        client.request.assert_has_calls(calls)
 
 
 if __name__ == "__main__":
